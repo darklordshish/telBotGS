4692dda8c794cd55cd20ba8dbdcd7b1108e2723a
Semion Shishkin <darkshish@gmail.com>
2022-09-14 17:14:28 +0300

    deleted coments

--- app/Main.hs
+++ app/Main.hs
@@ -16,8 +16,6 @@ import           Bot                         (mainBot)
 import           Schema                      (doMigration)
 import           DBConnection                (dbFilePath)
 
---connStr = "host=127.0.0.1 port=5432 user=semion dbname=magicbot password=semion " 
--- | latency(min)(cost -> min par) . par(?) . rps(max)(10-15%) . (length queue -> par,rps) . rps(sucsess )
 
 main :: IO ()
 main = do
